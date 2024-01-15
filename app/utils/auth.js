2cd601391f871c6eb92d43cf849e57881bfd230e
Uwancha <sito2alemu@gmail.com>
2024-01-15 22:03:50 +0300

    feat: Implement user authentication context using Context API Setup user authentication context. In this implementation, AuthProvider component manages user authentication state while useAuth hook provides easy access of user authentication state.

---
+++ app/utils/auth.js
@@ -0,0 +1,37 @@
+/**
+ * This file defines/implements the user authentication context using Context API
+ * It includes an AuthProvider component and a useAuth custom hook for easy access to authentication state.
+*/
+
+import { createContext, useContext, useState } from "react";
+
+// Create context
+const UserAuthContext = createContext();
+
+// AuthProvider manages user authentication state consuming Context API
+const AuthProvider = ({children}) => {
+    // State to hold user information
+    const [user, setUser] = useState(null);
+
+    return (
+        // UserAuthContext.Provider provides user state to its components
+        <UserAuthContext.Provider value={ { user, setUser } }>
+            { children }
+        </UserAuthContext.Provider>
+    )
+};
+
+// useAuth provides easy access to user authentication state
+const useAuth =  () => {
+    // Retrieve user state from context 
+    const context = useContext(AuthContext);
+
+    // Throw an error if context is not availble
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    };
+
+    return context;
+};
+
+export { AuthProvider, useAuth};
