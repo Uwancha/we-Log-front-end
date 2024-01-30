b17131156a94e06748244c3d11ab748076736590
Uwancha <sito2alemu@gmail.com>
2024-01-30 20:20:17 +0300

    feat: Add validation and handle errors from API and return respective messages

--- app/actions.js
+++ app/actions.js
@@ -1,30 +1,79 @@
 'use server'
 
-import { redirect } from "next/navigation"
-
-export async function register(formData) {
+export async function register(formState, formData) {
     const credentials = {
         name: formData.get('name'),
         email: formData.get('email'),
         password: formData.get('password'),
         confirmPassword: formData.get('confirmPwsd')
-    }
+    };
+   
+    if (credentials.password !== credentials.confirmPassword || credentials.confirmPassword === '') {
+        return {
+            success: false,
+            error: {
+                confirm: 'Password do not match'
+            }
+        }
+    };
 
+    if (credentials.name === '') {
+        return {
+            error: {
+                name: 'Name can not be empty and must be above 2 char'
+            }
+        }    
+    };
+
+    if (credentials.email === '') {
+        return {
+            error: {
+                name: 'Email is required!'
+            }
+        }    
+    };
+
+    if (credentials.password === '') {
+        return {
+            success: false,
+            error: {
+                password: 'Password must be at least 8 char'
+            }
+        }  
+    };
     const url = process.env.API_URL || 'http://localhost:4000/'
 
-    const res = await fetch(`${url}api/signup`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(credentials)
-    })
-
-    if (res.ok) {
-        redirect('/profile')
-    } else {
+    try {
+        const res = await fetch(`${url}api/signup`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+    
+        const data = await res.json();
+    
+        if (res.ok) {
+            return {
+                success: true
+            }
+        } else {
+            if(data.error === 'User already exists') {
+                return {
+                    error: {
+                        email: 'Email already used'
+                    }
+                }
+            } else if(data.errors) {
+                return {
+                    message: 'Invalid inputs'
+                }
+            }       
+        }
+    } catch (error) {
         return {
-            message: 'Invalid inputs'
+            message: 'Something went wrong. Try again!'
         }
     }
 }
