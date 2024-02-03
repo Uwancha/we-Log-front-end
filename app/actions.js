86ff5fa4e90466fb4ff1313bc6808688b4598098
Uwancha <sito2alemu@gmail.com>
2024-02-03 20:58:58 +0300

    feat: Add login server action to handle login Define login server action. Validate form values and send request to login endpoint. Handle response. On success save token and minmum user data in cookies

--- app/actions.js
+++ app/actions.js
@@ -1,4 +1,5 @@
 'use server'
+import { cookies } from "next/headers";
 
 export async function register(formState, formData) {
     const credentials = {
@@ -53,7 +54,6 @@ export async function register(formState, formData) {
         })
     
         const data = await res.json();
-    
         if (res.ok) {
             return {
                 success: true
@@ -70,6 +70,75 @@ export async function register(formState, formData) {
                     message: 'Invalid inputs'
                 }
             }       
+        }
+    } catch (error) {
+        return {
+            message: 'Something went wrong. Try again!'
+        }
+    }
+}
+
+export async function login (formState, formData) {
+    const credentials = {
+        email: formData.get('email'),
+        password: formData.get('password')
+    }
+  
+    if (credentials.email === '') {
+        return {
+            error: {
+                name: 'Email is required!'
+            }
+        }    
+    };
+  
+    if (credentials.password === '') {
+        return {
+            error: {
+                password: 'Password must be at least 8 char'
+            }
+        }  
+    };
+  
+    const url = process.env.API_URL || 'http://localhost:4000/'
+  
+    try {
+        const res = await fetch(`${url}api/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+  
+        const data = await res.json();
+  
+        if (data.message === 'Successfull login') {
+            cookies().set({
+                name: 'token' ,
+                value: data.token,
+                httpOnly: true,
+                path: '/',
+            })
+            cookies().set({
+                name: 'user',
+                value: data.user._id,
+                httpOnly: true,
+                path: '/',
+            })
+            return {
+                success: true,
+            }
+        } else {
+            if (data.message === 'Authentication failed') {
+                return {
+                    success: false, message: data.info.message
+                }
+            } 
+            
+            return {
+                message: 'Login failed. Please try again!'
+            } 
         }
     } catch (error) {
         return {
