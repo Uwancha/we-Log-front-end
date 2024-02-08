0d479b14d610f3bb98ecdbf72119e2c77006f1f5
Uwancha <sito2alemu@gmail.com>
2024-02-08 23:13:49 +0300

    feat: Fetch all posts inside Home server component Send request to fetch all posts that'll be displayed in the home page of the blog.

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,31 @@
-export default function Home() {
+// Send resquest to the API to get all posts that'll be displayed in the blog home page
+const getAllPosts = async () => {
+  try {
+    const url = process.env.API_URL || 'http://localhost:4000/'
+
+    const res = await fetch(`${url}api/posts`, {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+
+    if (res.ok) {
+      return res.json()
+    } else {
+      throw new Error('Failed to fetch!')
+    }
+
+  } catch (error) {
+    throw new Error('Something went wrong!')
+  }
+}
+
+export default async function Home() {
+  const data = await getAllPosts()
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <h1>Blog Home</h1>
     </main>
   )
