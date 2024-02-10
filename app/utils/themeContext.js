e5fd4b68c1b79b93e68ddde3980c0172e505d6e7
Uwancha <sito2alemu@gmail.com>
2024-02-10 23:04:23 +0300

    feat: Create theme context Create theme context using Context API. Create theme provider and a custom hook that provides theme state and setter

---
+++ app/utils/themeContext.js
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from "react";
+
+// Create theme context
+const ThemeContext = createContext(null);
+
+// Create theme context provider. This will provide theme context to those that it wraps
+export const ThemeContextprovider = (children) => {
+    const [theme, setTheme] = useState('light');
+
+    return (
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+};
+
+// This custom hook provide easy access to theme state and its setter
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+
+    if (!context) {
+        throw new Error('Context unavailable!')
+    }
+
+    return {
+        context
+    }
+}
+
